b3bac572713bf415f08b5fff15c1bb1216f75fbe
arthurknp <arthurpluviam@gmail.com>
2024-08-01 22:54:58 -0300

    arrumando coisas

--- submit.bat
+++ submit.bat
@@ -1,4 +1,3 @@
-del haxelib.zip
+::del haxelib.zip
 7z a haxelib.zip examples\ generator\ lib\ Build.xml haxelib.json LICENSE.md RayLib.hx README.md run.n -mx0 -xr!bin
-haxelib submit haxelib.zip
-del haxelib.zip
+::haxelib submit haxelib.zip
